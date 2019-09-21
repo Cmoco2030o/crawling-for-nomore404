3285ac726efd57b6dae6bd6391b4641960b8177f
Cmoco2030o <55534421+Cmoco2030o@users.noreply.github.com>
2019-09-21 23:45:55 +0300

    Ngkj
    G

--- وورد/setup.py
+++ وورد/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='no404-wordpress',
+    name='no404vj-wordpress',
     version='0.1.4',
     author='Kenji Nagahashi',
     author_email='kenji@archive.org',
